9715b21753782ddd3614fc3602e367a6028a6e6e
JagTheFriend <jagthefriend12@gmail.com>
2021-12-03 22:20:45 +0200

    Added icons

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -6,6 +6,8 @@ import "codemirror/mode/css/css";
 
 import { useState } from "react";
 import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default function Editor(props: {
     language: string;
@@ -22,7 +24,13 @@ export default function Editor(props: {
         <div className={`editor-container ${open ? "" : "collapsed"}`}>
             <div className="editor-title">
                 {displayName}
-                <button onClick={() => setOpen((prevOpen) => !prevOpen)}>O/C</button>
+                <button
+                    onClick={() => setOpen((prevOpen) => !prevOpen)}
+                    type="button"
+                    className="expand-collapse-btn"
+                >
+                    <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+                </button>
             </div>
             <ControlledEditor
                 onBeforeChange={handleChange}
