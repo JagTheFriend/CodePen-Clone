4006f6154b90566ad41d8e75189abd21569b06f7
JagTheFriend <jagthefriend12@gmail.com>
2021-12-03 21:06:35 +0200

    Created a new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import Editor from "./components/editor";
+
 function App() {
-    return <div>Hello</div>;
+    return (
+        <>
+            <div className="pane top-pane">
+                <Editor />
+                <Editor />
+                <Editor />
+            </div>
+            <div className="pane">
+                <iframe
+                    title="output"
+                    sandbox="allow-scripts"
+                    frameBorder="0"
+                    width="100%"
+                    height="100%"
+                />
+            </div>
+        </>
+    );
 }
 
 export default App;
